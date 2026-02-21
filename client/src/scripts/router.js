8b010af0b3414bb71c8bc0a73c264817a90df59a
Daynlight <danielstodulski000@gmail.com>
2026-02-22 00:32:45 +0100

    scripts folder

---
+++ client/src/scripts/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Auth from '../views/Auth.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/auth', component: Auth }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
